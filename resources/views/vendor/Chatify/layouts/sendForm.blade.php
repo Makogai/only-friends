67d130a99bb617ee83526223a6e6f80bc523cf37
Marko Pejanovic <marko.pejanovic@amplitudo.me>
2022-06-27 00:03:27 +0200

    changed chat send

--- resources/views/vendor/Chatify/layouts/sendForm.blade.php
+++ resources/views/vendor/Chatify/layouts/sendForm.blade.php
@@ -1,10 +1,8 @@
-<div class="px-6 w-full h-full">
-    <div class="messenger-sendCard w-full">
+    <div class="messenger-sendCard mb-4 border-[1px] nesto rounded-md w-full ml-[5%]" style="width: 90%!important;">
         <form id="message-form" method="POST" action="{{ route('send.message') }}" enctype="multipart/form-data">
             @csrf
-            <label class="border-t-[1px] rounded-l-md nesto"><span class="fas fa-paperclip"></span><input disabled='disabled' type="file" class="upload-attachment" name="file" accept=".{{implode(', .',config('chatify.attachments.allowed_images'))}}, .{{implode(', .',config('chatify.attachments.allowed_files'))}}" /></label>
-            <textarea readonly='readonly' name="message" class="m-send app-scroll border-l-transparent border-r-transparent" placeholder="Type a message.."></textarea>
-            <button disabled='disabled' class=" border-t-[1px] rounded-r-md nesto"><span class="fas fa-paper-plane"></span></button>
+            <label class="border-0 rounded-l-md nesto"><span class="fas fa-paperclip"></span><input disabled='disabled' type="file" class="upload-attachment" name="file" accept=".{{implode(', .',config('chatify.attachments.allowed_images'))}}, .{{implode(', .',config('chatify.attachments.allowed_files'))}}" /></label>
+            <textarea readonly='readonly' name="message" class="border-0 m-send app-scroll border-l-transparent border-r-transparent" placeholder="Type a message.."></textarea>
+            <button disabled='disabled' class="  rounded-r-md nesto"><span class="fas fa-paper-plane"></span></button>
         </form>
     </div>
-</div>
